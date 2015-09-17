93fd9ccd45d763007c27c5d152edae159c16f0fa
Brahmadut Ramkissoon <brahm.ramkissoon@gmail.com>
2015-09-16 20:19:52 -0400

    add private name instance variable to Program class

--- src/com/ramkissoon/brahm/Program.java
+++ src/com/ramkissoon/brahm/Program.java
@@ -1,9 +1,15 @@
 package com.ramkissoon.brahm;
 
 public class Program {
-
+	// instance variable, private variables use underscore
+	
+	private static String _name = "";
+	
 	public static void main(String[] args) {
 		System.out.println("Program started...");
+		_name = "Tom";
+		
+		System.out.println(_name);
 
 	}
 

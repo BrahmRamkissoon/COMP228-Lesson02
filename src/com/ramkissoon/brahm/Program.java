3e82ed7bb9e3ace7e27fb0edeaf7d4f4d0b0cdd7
Brahmadut Ramkissoon <brahm.ramkissoon@gmail.com>
2015-09-20 08:29:43 -0400

    Added input capabilities to Program class

--- src/com/ramkissoon/brahm/Program.java
+++ src/com/ramkissoon/brahm/Program.java
@@ -1,20 +1,34 @@
 package com.ramkissoon.brahm;
 
+import java.util.Scanner;
+
 public class Program {
 	public static Person person;	//reference to the Person class	
 	public static void main(String[] args) {
 		System.out.println("Program started...");
+		Scanner input = new Scanner(System.in);
+			
+		// Prompt user for Name
+		System.out.print("Enter your name: ");
+		String myName = input.nextLine();
 		
-		person = new Person(46);
+		// Prompt user for Gender
+		System.out.print("Enter your gender: ");
+		String myGender = input.nextLine();
 		
-		person.setName("Tom");
-		person.setGender("Male");
+		// Prompt user for Age
+		System.out.print("Enter your age: ");
+		int myAge = input.nextInt();
+				
+		person = new Person(myAge);
 		
-		System.out.println(person.getName());
+		person.setName(myName);
+		person.setGender(myGender);
 		
 		person.runs();
 		
-		System.out.printf("%s is %d years old",person.getName(), person.getAge());
+		System.out.printf("%s is %d years old and is %s",
+				person.getName(), person.getAge(), person.getGender());
 		
 	}
 
